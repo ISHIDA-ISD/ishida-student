63eda98b6bd2b02e875dd2122f94e8110ca640d6
ISHIDA-ISD <XXXishida999@gmail.com>
2025-02-23 08:58:11 +0000

    1.1.8

---
+++ main/sever.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 3000;
+
+let users = []; // 簡単なメモリ内データベース（実際にはデータベースを使用することを推奨）
+
+app.use(bodyParser.json());
+
+app.post('/register', (req, res) => {
+    const { username, password } = req.body;
+    users.push({ username, password });
+    res.send('登録完了');
+});
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    const user = users.find(u => u.username === username && u.password === password);
+    if (user) {
+        res.send('ログイン成功');
+    } else {
+        res.status(401).send('ユーザー名またはパスワードが間違っています');
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
